a14789c029777a069cfd4310c6daaef0bb754b05
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-02-07 05:25:46 +0900

    Update BasicSyntaxes.ml

--- BasicSyntaxes.ml
+++ BasicSyntaxes.ml
@@ -471,7 +471,7 @@ let t =
     /    \
    2      3
   / \    / \
- l   l  l   l
+ L   L  L   L
 *)
 
 let rec size = function
@@ -607,3 +607,78 @@ let t = Node (1,
 add1 t;;
 sum t;;
 t |> add1 |> sum;;
+
+(* modules and structures *)
+
+module MyList = struct
+
+  type 'a mylist =
+    | Nil
+    | Cons of 'a * 'a mylist
+
+  let rec map f = function
+    | Nil -> Nil
+    | Cons (h, t) -> Cons (f h, map f t)
+
+end
+
+module Tree = struct
+
+  type 'a tree =
+    | Leaf
+    | Node of 'a * 'a tree * 'a tree
+
+  let rec map f = function
+    | Leaf -> Leaf
+    | Node (v, l, r) -> Node (f v, map f l, map f r)
+
+end
+
+let lst = MyList.map succ (Cons (1, Nil))
+
+let t : int Tree.tree = Node (1, Leaf, Leaf)
+
+(* functional stacks *)
+
+module MyStack = struct
+  
+  type 'a stack = (* list *)
+    | Empty (* nil *)
+    | Entry of 'a * 'a stack (* cons *)
+  
+  let empty = Empty
+
+  let push x s =
+    Entry (x, s)
+  
+  let peek = function (*  *)
+    | Empty -> failwith "Empty"
+    | Entry (x, _) -> x
+  
+  let pop = function
+    | Empty -> failwith "Empty"
+    | Entry (_, s) -> s
+  
+end
+
+module ListStack = struct
+  
+  type 'a stack = 'a list
+  
+  let empty = []
+
+  let push x s = x :: s
+  
+  let peek = function
+    | [] -> failwith "Empty"
+    | x :: _ -> x
+  
+  let pop = function
+    | [] -> failwith "Empty"
+    | _ :: s -> s
+  
+end
+
+let s = ListStack.empty;;
+let s' = ListStack.push 1 s;;
+let x = ListStack.peek s';;
