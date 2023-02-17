85ebb64302651b98fd5ea1cf7ef49f114c92b7f6
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-02-18 00:26:32 +0900

    Create BooleanExpression.ml

---
+++ BooleanExpression.ml
@@ -0,0 +1,18 @@
+type 'a expr =
+| True
+| False
+| And  of 'a expr * 'a expr
+| Or   of 'a expr * 'a expr
+| Not  of 'a expr
+| Base of 'a
+;;
+let rec eval eval_base expr =
+  let eval' x = eval eval_base x in
+  match expr with
+  | True       -> true
+  | False      -> false
+  | Base base  -> eval_base base
+  | And (x, y) -> eval' x && eval' y
+  | Or  (x, y)  -> eval' x && eval' y
+  | Not x -> not (eval' x)
+;;
