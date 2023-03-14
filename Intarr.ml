fb11936d410aa3bf441435a3a28bf028c01bedce
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-03-15 00:08:07 +0900

    Update and rename intarr.ml to Intarr.ml

---
+++ Intarr.ml
@@ -0,0 +1,4 @@
+open Scanf
+;;
+let rec intarr = function | 1 -> [scanf "%d\n" @@ fun n -> n] | x -> scanf "%d " @@ fun n -> (n :: intarr @@ x - 1)
+;;
--- intarr.ml
+++
@@ -1,4 +0,0 @@
-open Scanf
-;;
-let rec intarr = function | 1 -> [scanf "%d\n" (fun n -> n)] | x -> scanf "%d " (fun n -> n :: intarr (x-1))
-;;
